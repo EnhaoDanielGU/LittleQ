503d2844362fcadedeb2224a42fb69ff65405b30
GU Enhao <43510611+EnhaoDanielGU@users.noreply.github.com>
2018-09-30 01:17:50 +0800

    Delete auxiliary_wk2.py

--- auxiliary_wk2.py
+++
@@ -1,9 +0,0 @@
-def generate_bar(data):
-    ## Data is a pandas dataframe
-    import pandas as pd
-
-    OHLC = pd.DataFrame(data, columns = ['open', 'high', 'low', 'close', 'volume_ave'])
- 
-   
-    
-    return OHLC
